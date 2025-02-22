65f27cdacbdb8cf8f853a78c2d0720b5cb1d6fad
Daniel Cocos <dev.danielcocos@gmail.com>
2025-02-22 16:42:49 +0000

    Moved screenshots to a function

--- utils/screenshot.py
+++ utils/screenshot.py
@@ -1,8 +1,21 @@
 from PIL import ImageGrab
+import tkinter as tk
 import os
-PATH = os.path.join("assets", "image.png")
 
-def take_screenshot(PATH):
-    screenshot = ImageGrab.grab()
-    screenshot.save(PATH, "PNG")
-    screenshot.close()
+
+
+class ScreenShot:
+    def __init__(self, X, Y) -> None:
+        self.PATH = os.path.join("assets", "image.png")
+        self.X = X
+        self.Y = Y
+
+    def take_screenshot(self):
+        screenshot = ImageGrab.grab()
+        screenshot.save(self.PATH, "PNG")
+        screenshot.close()
+
+    def take_screenshot_area(self):
+        screenshot = ImageGrab.grab()
+        screenshot.save(self.PATH, "PNG")
+        screenshot.close()
